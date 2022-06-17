78c9f5b9a1c41f4173bf094c23aac86eb539b04e
CaioRuizz <caio.ruiz00@gmail.com>
2022-06-17 16:55:02 -0300

    uploading code

---
+++ setup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+pip3 install -r requirements.txt
+python3 -c "import nltk;nltk.download('stopwords')"
---
+++ summarizer/__init__.py
@@ -0,0 +1,116 @@
+from nltk.corpus import stopwords
+from nltk.cluster.util import cosine_distance
+import numpy as np
+import networkx as nx
+
+
+def _read_article(text):
+    filedata = text.split('\n')
+    article = filedata[0].split(". ")
+    sentences = []
+
+    for sentence in article:
+        # print(sentence)
+        sentences.append(sentence.replace("[^a-zA-Z]", " ").split(" "))
+    sentences.pop() 
+    
+    return sentences
+
+def _sentence_similarity(sent1, sent2, stopwords=None):
+    if stopwords is None:
+        stopwords = []
+ 
+    sent1 = [w.lower() for w in sent1]
+    sent2 = [w.lower() for w in sent2]
+ 
+    all_words = list(set(sent1 + sent2))
+ 
+    vector1 = [0] * len(all_words)
+    vector2 = [0] * len(all_words)
+ 
+    # build the vector for the first sentence
+    for w in sent1:
+        if w in stopwords:
+            continue
+        vector1[all_words.index(w)] += 1
+ 
+    # build the vector for the second sentence
+    for w in sent2:
+        if w in stopwords:
+            continue
+        vector2[all_words.index(w)] += 1
+ 
+    return 1 - cosine_distance(vector1, vector2)
+ 
+def _build_similarity_matrix(sentences, stop_words):
+    # Create an empty similarity matrix
+    similarity_matrix = np.zeros((len(sentences), len(sentences)))
+ 
+    for idx1 in range(len(sentences)):
+        for idx2 in range(len(sentences)):
+            if idx1 == idx2: #ignore if both are same sentences
+                continue 
+            similarity_matrix[idx1][idx2] = _sentence_similarity(sentences[idx1], sentences[idx2], stop_words)
+
+    return similarity_matrix
+
+
+def _normalize(txt):
+    txt = txt.replace('\n', '')
+    return txt
+
+def summarize_paragraph(text, top_n=5):
+    text = _normalize(text)
+
+    stop_words = stopwords.words('portuguese')
+    summarize_text = []
+
+    # Step 1 - Read text anc split it
+    sentences =  _read_article(text)
+
+    # Step 2 - Generate Similary Martix across sentences
+    sentence_similarity_martix = _build_similarity_matrix(sentences, stop_words)
+
+    # Step 3 - Rank sentences in similarity martix
+    sentence_similarity_graph = nx.from_numpy_array(sentence_similarity_martix)
+    scores = nx.pagerank(sentence_similarity_graph)
+
+    # Step 4 - Sort the rank and pick top sentences
+    ranked_sentence = sorted(((scores[i],s) for i,s in enumerate(sentences)), reverse=True)    
+    # print("Indexes of top ranked_sentence order are ", ranked_sentence)    
+
+    for i in range(min(top_n, len(ranked_sentence))):
+        summarize_text.append(" ".join(ranked_sentence[i][1]))
+
+    # Step 5 - Offcourse, output the summarize texr
+    # print("Summarize Text: \n", ". ".join(summarize_text))
+
+    return '. '.join(summarize_text)
+
+def summarize_text(text, group_by=1):
+    summarized_paragraphs = []
+
+    text = text.replace('\n\n', '\n')
+
+    paragraphs : list[str] = text.split('\n')
+
+    non_empty_paragraphs = list(filter(lambda x: x.strip() != '', paragraphs))
+
+    groups = []
+
+    for count, paragraph in enumerate(non_empty_paragraphs):
+        print(count, group_by)
+        if count % group_by == 0:
+            groups.append(paragraph)
+        else:
+            groups[-1] += f'\n{paragraph}'
+
+    print(len(groups))
+
+    for paragraph in groups:
+        # print(paragraph)
+        summarized_paragraphs.append(summarize_paragraph(paragraph, group_by))
+
+    summarized_paragraphs = list(filter(lambda x: x.strip() != '', summarized_paragraphs))
+
+    return '.\n'.join(summarized_paragraphs)
